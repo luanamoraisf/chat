d4b85072abc70c5cb901cb8b0837c34cd6fac663
luanamoarisf <luanaflorenciopc@gmail.com>
2024-09-30 17:42:29 -0300

    change welcome text

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="text-4xl font-serif mt-28">Olá, seja bem-vindo ao Chat
+      <p className="text-4xl font-serif mt-28">Olá, seja bem-vindo ao Chat!
       </p>
       <button onClick={handleRedirect} className="bg-violet-500 border-8 border-violet-500 hover:shadow-2xl hover:shadow-violet-900/50 rounded-lg text-4xl text-white mb-96 border-spacing-7">Entrar</button>
     </main>
