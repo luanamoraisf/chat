038e3a06f4e0d57159df8785d8a8120d812a9540
luanamoarisf <luanaflorenciopc@gmail.com>
2024-11-30 21:59:00 -0300

    define a presence channel

--- backend/routes/channels.php
+++ backend/routes/channels.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('messages', function ($user) {
+    return ['id' => $user->id, 'name' => $user->name];
 });
