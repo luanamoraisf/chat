8477a2f88b28c9a51d1d82c44da140d251cad19a
luanamoraisf <luanaflorenciopc@gmail.com>
2024-12-23 18:42:35 -0300

    configures event to be executed

--- backend/app/Events/NewMessage.php
+++ backend/app/Events/NewMessage.php
@@ -10,22 +10,25 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class NewMessage
+class NewMessage implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+    public $user;
 
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct($message, $user)
     {
-        //
+        $this->message = $message;
+        $this->user = $user;
     }
 
     /**
      * Get the channels the event should broadcast on.
      *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
      */
     public function broadcastOn()
     {
